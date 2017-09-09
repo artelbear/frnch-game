2c19e676730bffdb300a25c14199c0a69d0965e9
Nikolay <kn@localhost.localdomain>
2017-09-09 12:44:27 +0300

    remove unstable content

--- main.lua
+++ main.lua
@@ -1,5 +1,23 @@
 function love.load()
+    time = 1
+    grid = {
+        w = 10,
+        h = 7
+    }
+    fonts = {
+        h = love.graphics.newFont("font/Leokadia.ttf", 30),
+        b = love.graphics.newFont("font/Sunday-Regular.otf", 30),
+        f = love.graphics.newFont("font/Pecita.otf", 30)
+    }
+end
+
+function love.update(dt)
 end
 
 function love.draw()
+    love.graphics.setFont(fonts.h)
+    love.graphics.print("text", 0, 0)
+    love.graphics.setFont(fonts.b)
+    love.graphics.print("text", 0, 100)
+    love.graphics.setFont(fonts.f)
 end
--- video/readme.txt
+++
@@ -1,4 +0,0 @@
-Author: Elcondor Film
-Author webpage: https://vimeo.com/elcondorfilm 
-Licence: ATTRIBUTION LICENSE 3.0 (http://creativecommons.org/licenses/by/3.0/us/)
-Downloaded at Mazwai.com
